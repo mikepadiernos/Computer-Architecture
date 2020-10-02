a1e62e64d08e71d11631379b49e47671246049ef
mikepadiernos <mike.padiernos@gmail.com>
2020-10-02 14:20:52 -0400

    [FEATURE] completion / cpu
    - mvp, added ram_read & ram_write
    - coded run

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,29 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        """
+        Construct a new CPU.
+        """
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.reg[7] = 0xF4
+        self.pc = 0
+        self.running = True
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self, pc, value):
+        self.ram[pc] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +35,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +64,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +78,19 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+            execute = self.ram_read(self.pc)
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+
+            if execute == HLT:
+                self.running = False
+            elif execute == LDI:
+                self.reg[op_a] = op_b
+                self.pc += 3
+            elif execute == PRN:
+                value = self.reg[op_a]
+                print(f"The value is {value}.")
+                self.pc += 2
+            else:
+                sys.exit(1)
