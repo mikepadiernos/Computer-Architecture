9865c9f9ccf691faf3aa59345d08bf4df7f8a5d6
mikepadiernos <mike.padiernos@gmail.com>
2020-10-06 09:14:56 -0400

    [FEATURE] completion / cpu
    - converted machine code from hard coding

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -25,25 +25,29 @@ class CPU:
         self.ram[pc] = value
 
     def load(self):
+
         """Load a program into memory."""
+        if len(sys.argv) != 2:
+            print('insufficient arguments')
+            sys.exit(1)
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line_split = line.split('#')
+                    num = line_split[0].strip()
+                    if num == '':
+                        continue
+                    try:
+                        self.ram_write(address, int(num, 2))
+                    except:
+                        print('unable to convert string to integer')
+                    address += 1
+        except:
+            print('file not found')
+            sys.exit(1)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
